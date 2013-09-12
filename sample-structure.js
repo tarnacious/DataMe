a75393000266d9733ccdf7ab0c27b7f8ddc05bb5
Andy Giefer <andyg@Andys-MacBook-Pro-2.local>
2013-09-12 14:46:00 +0200

    Initial data structure

---
+++ sample-structure.js
@@ -0,0 +1,37 @@
+[{
+	"timestamp": "2013-07-09T13:00:05Z",
+	"geolocation": {
+		"lat":50.000,
+		"long":-6
+	},
+	"source": "twitter",
+	"data": {
+			"text": "swfsdfsd",
+			"userimage": "http://swfsdfsd.jpg"
+	}		
+},
+{
+	"timestamp": "2013-07-09T13:05:05Z",
+	"geolocation": {
+		"lat":50.000,
+		"long":-6
+	},
+	"source": "flickr",
+	"data": {
+			"imageUrl": "http://swfsdfsd.jpg",
+			"userimage": "sdsd.jpg"
+	}
+},
+{
+	"timestamp": "2013-07-09T13:10:05Z",
+	"geolocation": {
+		"lat":50.000,
+		"long":-6
+	},
+	"source": "wikipedia",
+	"data": {
+			"imageUrl": "http://swfsdfsd.jpg",
+			"userimage": "sdsd.jpg"
+	}
+},
+]
