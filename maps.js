642714df6a26da41f0a7466e503e84c7400b9bec
Tarn Barford <tarn@tarnbarford.net>
2013-09-13 10:55:00 +0200

    add play/stop

--- maps.js
+++ maps.js
@@ -1,4 +1,6 @@
-var map;
+var map, current_time;
+var animating = false;
+
 var twitter_image = {
     url: '../images/twitter.png',
     // This marker is 20 pixels wide by 32 pixels tall.
@@ -38,8 +40,7 @@ function add_tweets(data) {
         if (point.source == 'twitter') {
             var marker = new google.maps.Marker({
                 icon: twitter_image,
-                position: latlng,
-                title:"Hello World!"
+                position: latlng
             });
             marker.setMap(map);
             var infowindow = new google.maps.InfoWindow({
@@ -52,8 +53,7 @@ function add_tweets(data) {
         if (point.source == 'wikipedia') {
             var marker = new google.maps.Marker({
                 icon: wikipedia_image,
-                position: latlng,
-                title:"Hello World!"
+                position: latlng
             });
             marker.setMap(map);
             var infowindow = new google.maps.InfoWindow({
@@ -128,10 +128,21 @@ function initialize() {
             add_lines(data);
         });
 
-        $("#action").click(function() {
+        $("#stop").click(function() {
+            animating = false;
+        });
+            
+
+        $("#play").click(function() {
+            if (animating == true) {
+                return;
+            };
+            animating = true;
             start = parseInt(data[0].timestamp, 10);
             (function animloop(time){
-              window.requestAnimationFrame(animloop);
+              if (animating == true) {  
+                  window.requestAnimationFrame(animloop);
+              }
               start = start + 100;
               index = find_index(start, data);
 
@@ -153,34 +164,6 @@ function initialize() {
             })();
         });
 
-        $("#action2").click(function() {
-            function animate_all(coordinates, complete) {
-                if (coordinates.length == 1) {
-                    complete();
-                    return;
-                };
-                console.log(coordinates[0].lat() - coordinates[1].lat(), coordinates[0].lng() - coordinates[1].lng()); 
-                if ((Math.abs(coordinates[0].lat() == coordinates[1].lat()) > 0.01)  && 
-                    (Math.abs(coordinates[0].lng() == coordinates[1].lng()) > 0.01)) {
-                    console.log("GOOD");
-                    //var bounds = new google.maps.LatLngBounds(coordinates[0], coordinates[1]);
-                    //map.fitBounds(bounds);
-                    map.setCenter(coordinates[0]);
-                    map.setZoom(12);
-                } else {
-                    map.setCenter(coordinates[0]);
-                    map.setZoom(12);
-                    console.log("SKIPPING");
-                }
-
-                animate(coordinates[0], coordinates[1], marker, function() {
-                    animate_all(coordinates.slice(1), complete);
-                });
-            };
-            animate_all(lineCoordinates, function() {
-                alert("ALL DONE");
-            })
-        });
     });
 }
 
@@ -199,27 +182,6 @@ function go_to_time(time, data) {
     return data[index];
 }
 
-animate = function(start, finish, marker, complete) {
-    var steps = 80;
-    var step_lat = (start.lat() - finish.lat()) / steps;
-    var step_lng = (start.lng() - finish.lng()) / steps;
-    var current = start;
-
-
-    (function animloop(time){
-      if (steps > 0) {  
-          window.requestAnimationFrame(animloop);
-      } else {
-          complete();
-          return;
-      };
-      steps -= 1;
-      var lat = current.lat() - step_lat;
-      var lng = current.lng() - step_lng;
-      current = new google.maps.LatLng(lat, lng);
-      marker.setPosition(current);
-    })();
-}
 
 $(function() {
     google.maps.event.addDomListener(window, 'load', initialize);
